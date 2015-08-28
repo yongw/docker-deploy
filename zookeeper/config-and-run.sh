07350a467450e206c0a47aba3f165a8b50a86b6e
Yong Wang <yongw@live.com>
2015-08-28 15:10:39 +0800

    Updated script to get other zookeeper server nodes information

--- zookeeper/config-and-run.sh
+++ zookeeper/config-and-run.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 
-echo "$SERVER_ID / $MAX_SERVERS" 
-if [ ! -z "$SERVER_ID" ] && [ ! -z "$MAX_SERVERS" ]; then
-  echo "Starting up in clustered mode"
-  echo "" >> /opt/zookeeper/conf/zoo.cfg
-  echo "#Server List" >> /opt/zookeeper/conf/zoo.cfg
+echo "$ZK_SERVER_ID / $MAX_SERVERS" 
+if [ ! -z "$ZK_SERVER_ID" ] && [ ! -z "$MAX_SERVERS" ]; then
+  echo "$ZK_SERVER_ID" > /opt/zookeeper/data/myid
+  #Find the servers exposed in env.
   for i in $( eval echo {1..$MAX_SERVERS});do
-    echo "server.$i=zookeeper-$i:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
-  done
-  cat /opt/zookeeper/conf/zoo.cfg
 
-  # Persists the ID of the current instance of Zookeeper
-  echo ${SERVER_ID} > /opt/zookeeper/data/myid
-  else
-	  echo "Starting up in standalone mode"
+    HOST=`envValue ZK_PEER_${i}_SERVICE_HOST`
+    PEER=`envValue ZK_PEER_${i}_SERVICE_PORT`
+    ELECTION=`envValue ZK_ELECTION_${i}_SERVICE_PORT`
+
+    if [ "$ZK_SERVER_ID" = "$i" ];then
+      echo "server.$i=0.0.0.0:2888:3888" >> conf/zoo.cfg
+    elif [ -z "$HOST" ] || [ -z "$PEER" ] || [ -z "$ELECTION" ] ; then
+      #if a server is not fully defined stop the loop here.
+      break
+    else
+      echo "server.$i=$HOST:$PEER:$ELECTION" >> conf/zoo.cfg
+    fi
+
+  done
 fi
 
 exec /opt/zookeeper/bin/zkServer.sh start-foreground
